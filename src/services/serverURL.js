f22465f97121a39a5b755c37f9341f0aac3fd3b3
sonykuriakoseantony <sonykuriakoseantony@gmail.com>
2025-11-07 20:13:55 +0530

    server url changed to production url

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,5 @@
-const serverURL = "http://localhost:3000"
+//const serverURL = "http://localhost:3000"
+
+const serverURL = "https://resume-builder-server-4v3t.onrender.com"
+
 export default serverURL
